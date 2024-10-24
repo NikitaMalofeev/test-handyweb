3a9cca475e0ab142edd98c67c3c5787ca5eaf5e0
NikitaMalofeev <nikita.m.edu@gmail.com>
2024-10-24 19:30:28 +0500

    fix ' request

--- src/shared/config/catalogSelectOptions.ts
+++ src/shared/config/catalogSelectOptions.ts
@@ -1,7 +1,7 @@
 export const catalogSelectOptions = [
     { value: "", label: "All Categories" },
-    { value: "mens clothing", label: "Mens Clothing" },
-    { value: "womens clothing", label: "Womens Clothing" },
+    { value: "men's clothing", label: "Men's Clothing" },
+    { value: "women's clothing", label: "Women's Clothing" },
     { value: "jewelery", label: "Jewelery" },
     { value: "electronics", label: "Electronics" },
 ]
